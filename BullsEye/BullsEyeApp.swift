1c2cdd0c2b168911a94af4eaad3184b27bc27a96
Babur Farooq <babur.farooq@gmail.com>
2021-04-01 16:34:56 -0400

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Babur Farooq on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
